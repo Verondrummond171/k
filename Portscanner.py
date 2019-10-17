0a29de7a1aebec9c7ab21a796ed6452bd6309b24
Verondrummond171 <56696878+Verondrummond171@users.noreply.github.com>
2019-10-17 16:47:11 -0500

    Create Portscanner.py

---
+++ Portscanner.py
@@ -0,0 +1,18 @@
+import socket
+
+class bcolors:
+       GREEN = '\033[92m'
+def banner():
+   print(bcolors.GREEN+()+made by Dujohn+()+)
+
+s = socket.socket(Socket.AF_INET,SOCKET.SOCK_STREAM)
+
+host = "input(" enter the IP":)
+port = int("input(enter port:")
+
+def port scanner():
+if s.connect_exc host,port()):
+print("the port is open")
+else:
+print("the port is closed")
+port scanner(port)
